dff20d988449ab11d698e37501f9101b989c9645
Leonardo Rifeli <leonardorifeli@gmail.com>
2016-06-05 22:04:45 -0300

    Fixed the unserialize error

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new RequestBundle\RequestBundle(),
             new MessageBundle\MessageBundle(),
-            new ApiBundle\ApiBundle(),
+            new ApiBundle\ApiBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
